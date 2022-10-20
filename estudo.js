a39789e0a2e86532a3b5e4e3c2054c712f02b9c5
laranjaatomica <108937367+laranjaatomica@users.noreply.github.com>
2022-10-20 11:15:13 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -40,3 +40,25 @@ function total(){
     }
     document.write("Resultado: " + r);
 }
+function SomaNota(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+
+    let r =  Number(n1)+Number(n2)+Number(n3)+Number(n4);
+
+    document.getElementById("resul").innerHTML = "Soma: " + r;
+
+}
+function media(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    
+    let r =  Number(n1)+Number(n2)+Number(n3)+Number(n4);
+
+    document.getElementById("resul").innerHTML = "Resultado " + r /4;
+   
+}
