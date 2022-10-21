df2b9686e29065041f34fb79caa25cda2a4a4683
laranjaatomica <108937367+laranjaatomica@users.noreply.github.com>
2022-10-21 09:57:33 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -28,17 +28,39 @@ function mostratabuada(){
         document.write("O valor do i " + i + "<br>");
     }
 }
+function moeda(atual){
+    
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
 function total(){
     let v = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
+    if (!Number (v)){
+        alert("O campo valor deve ser númreros.");
+        document.getElementById("valor").value = "";
+        document.getElementById("valor").focus();
+        return;
+    }
+    if (!Number (j)){
+        alert("O campo juros deve ser númreros.");
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+        return;
+    }
+    if (!Number (t)){
+        alert("O campo N.meses deve ser númreros.");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return;
+    }
     let r = 0; 
     for(let i =1; i <=t; i++){
         r = v * (1 + (j/100));
-        document.write("Mẽs " + i + " - valor: " + r + "<br>");
+        document.write("Mẽs " + i + " - valor: " + moeda(r) + "<br>");
         v = r;
     }
-    document.write("Resultado: " + r);
+    document.write("Resultado: " + moeda(r));
 }
 function SomaNota(){
     let n1 = document.getElementById("b1").value;
@@ -61,4 +83,14 @@ function media(){
 
     document.getElementById("resul").innerHTML = "Resultado " + r /4;
    
+}
+function passa(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    
+    let r =  Number(n1)+Number(n2)+Number(n3)+Number(n4);
+
+    document.getElementById("resul").innerHTML = "Resultado " + (r -24);
 }
